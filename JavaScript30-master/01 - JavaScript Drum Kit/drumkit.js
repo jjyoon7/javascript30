caf99fdba60d70ebddb16f3c1aae8ed77ae0d71b
jjyoon7 <jay.yoon7@gmail.com>
2019-11-21 17:20:17 +0100

    you can check which css property has been changed by checking event property in dev console

--- JavaScript30-master/01 - JavaScript Drum Kit/drumkit.js
+++ JavaScript30-master/01 - JavaScript Drum Kit/drumkit.js
@@ -1,16 +1,29 @@
-function handleKeyDown(e) {
+const keys = document.querySelectorAll('.key');
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function playSound(e) {
     // "${e.keyCode}" is ES6 syntax, where it is chosing the specific audio element 
     //  where the data-key value matches with the e.keyCode.
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+    //if there is no audio matching with the key down, then stop the function.
+    if(!audio) return;
     audio.play();
     audio.currentTime = 0;
     key.classList.add('playing');
 }
 
+
+
 function init() {
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', playSound);
 }
 
 init();
