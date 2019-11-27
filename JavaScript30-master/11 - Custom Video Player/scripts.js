26a26a0c03623db2e681d1719c9954f3836f018d
jjyoon7 <jay.yoon7@gmail.com>
2019-11-27 19:33:02 +0100

    make sure to check the data type, if it is string or number. In handleSkip function, you need to parseFloat() the 'this.dataset.skip' in order to use it with the 'video.currentTime'

--- JavaScript30-master/11 - Custom Video Player/scripts.js
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -3,8 +3,8 @@ const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 function handleVideo() {
     // console.log('play')
@@ -22,8 +22,19 @@ function handleButtons() {
     toggle.textContent = icon;
 }
 
+function handleSkip() {
+    // console.log(this.dataset.skip);
+    //because it is a string, you need to parse the number.
+    const skipAmount = parseFloat(this.dataset.skip);
+    // console.log(skipAmount);
+    video.currentTime += skipAmount;
+    // this.skip
+}
+
 video.addEventListener('click', handleVideo);
 video.addEventListener('play', handleButtons);
 video.addEventListener('pause', handleButtons);
 
 toggle.addEventListener('click', handleVideo);
+
+skipButtons.forEach( skip => { skip.addEventListener('click', handleSkip)})
